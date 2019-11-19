ffacb01d463951c2c589ce1280659dfa87cca87f
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-19 16:15:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,46 @@
 class Artist
- attr_accessor :name, :songs
+  attr_accessor :name, :songs
+  @@all = []
 
- @@all = []
-
- def  initialize(name)
-   @name = name
-    @@all
+  def initialize(name)
+    @name = name
+    save
   end
-end
+
+  def save
+    @@all << self
+  end
 
   def self.all
     @@all
- end
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    @songs = Song.all.select { |song| song.artist == self  }
+  end
+
+
+  def add_song_by_name(song_name)
+    Song.new(song_name).artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    if self.all.find {|artist| artist.name == artist_name} == nil
+      Artist.new(artist_name)
+    else
+      self.all.find {|artist| artist.name == artist_name}
+    end
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
 end
